2cf54ed116839a925102c07069011eba203b534b
Vicenkks <vicentejavierhuilcaman@gmail.com>
2026-08-28 12:44:45 -0400

    feat: GET one user standarized

--- app/students/students_controller.py
+++ app/students/students_controller.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from app.pets.pets_service import pets_service
 from app.students.students_schemas import CreateStudentDto, Student, UpdateStudentDto
+from app.students.students_service import students_service
 
 from app.utils.apiResponse import StandardResponse
 router = APIRouter(prefix="/api/students", tags=["Students"])
@@ -13,9 +14,13 @@ def find_all() -> list[Student]:
 
 
 @router.get("/{student_id}", response_model = StandardResponse[Student])
-def find_by_id(student_id: str) -> Student:
-    return students_service.find_by_id(student_id)
-
+def find_by_id(student_id: str) -> StandardResponse[Student]:
+    return StandardResponse(
+        success = True,
+        statusCode = 200,
+        message = "Users obtained successful",
+        data = students_service.find_by_id(student_id)
+    )
 
 @router.post("", status_code=201)
 def create(body: CreateStudentDto) -> Student:
