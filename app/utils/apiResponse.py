fc7006136f9336f2173ae530fe2a4e1533845c09
Vicenkks <vicentejavierhuilcaman@gmail.com>
2026-08-28 12:45:46 -0400

    feat: Utils standard

--- app/utils/apiResponse.py
+++ app/utils/apiResponse.py
@@ -1,11 +1,15 @@
-from datetime import datetime
-from pydantic import BaseModel, EmailStr
+from datetime import datetime, timezone
+from typing import Generic, TypeVar
+from pydantic import BaseModel, Field
+
+DataT = TypeVar("DataT")
 
 
-class StudentResponse(BaseModel):
-    id: str
-    name: str
-    email: EmailStr
-    age: int
-    createdAt: datetime
-    updatedAt: datetime
+class StudentResponse(BaseModel, Generic[DataT]):
+    success: bool = True
+    statusCode: int = 200
+    message: str = "Ok"
+    data: DataT | None = None
+    timestamp: datetime = Field(
+        default_factory=lambda: datetime.now(timezone.utc)
+    )
